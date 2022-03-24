a443a297262efa789584a9acdc2ac48e66e42130
Documenter.jl <documenter@juliadocs.github.io>
2022-03-24 20:40:18 +0000

    build based on d4c3d45

--- dev/literate/bas.jl
+++ dev/literate/bas.jl
@@ -5,7 +5,7 @@ nothing #hide
 
 # Some example patterns of the Bars and Stripes (BAS) ensemble.
 
-fig = Makie.Figure(resolution = (600,600))
+fig = Makie.Figure(resolution = (800,600))
 for col in 1:7, row in 1:4
     ax = Makie.Axis(fig[row, col], yreversed=true)
     Makie.image!(ax, bas(10, 10), colorrange=(1,0))
